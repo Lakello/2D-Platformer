c57e4ab85302d38bdd5f819219ab68a98d89e52c
Lakello <59009343+Lakello@users.noreply.github.com>
2023-03-14 21:42:44 +0700

    jump

--- Assets/Source/Scripts/Player/PlayerJumping.cs
+++ Assets/Source/Scripts/Player/PlayerJumping.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 
 public class PlayerJumping : MonoBehaviour
 {
-    [SerializeField, Range(1, 10)] private float _height = 3;
+    [SerializeField, Range(1f, 10f)] private float _height = 3f;
+    [SerializeField, Range(0.1f, 10f)] private float _timeToReachHeight = 1f;
 
     private Rigidbody2D _selfRigidbody;
+    private float _startPositionY;
+
+    private float _endPositionY => _startPositionY + _height;
 
     private void Start()
     {
@@ -14,10 +18,31 @@ public class PlayerJumping : MonoBehaviour
 
     public void OnJumping()
     {
-        float abs = Mathf.Abs(_height * Physics2D.gravity.y);
+        StopAllCoroutines();
 
-        Vector2 force = Mathf.Sqrt(abs) * Vector2.up;
+        StartCoroutine(Jump());
+    }
 
-        _selfRigidbody.AddForce(force, ForceMode2D.Impulse);
+    private IEnumerator Jump()
+    {
+        _startPositionY = _selfRigidbody.position.y;
+
+        while (_selfRigidbody.position.y <= _endPositionY)
+        {
+            float targetVelocity = Mathf.Lerp(_startPositionY,
+                                                _endPositionY,
+                                                _timeToReachHeight);
+
+            SetVelocity(targetVelocity);
+
+            yield return null;
+        }
+
+        SetVelocity(0);
+    }
+
+    private void SetVelocity(float value)
+    {
+        _selfRigidbody.velocity = new Vector2(_selfRigidbody.velocity.x, value);
     }
 }
--- Assets/Source/Scripts/Player/PlayerMovement.cs
+++ Assets/Source/Scripts/Player/PlayerMovement.cs
@@ -2,8 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField, Range(50, 500)] private float _force = 365;
-    [SerializeField, Range(0.5f, 15f)] private float _maxSpeed = 5f;
+    [SerializeField, Range(1, 500)] private float _force = 365;
 
     private Rigidbody2D _selfRigidbody;
 
@@ -14,17 +13,7 @@ public class PlayerMovement : MonoBehaviour
 
     public void OnMoveing(float horizontalInput)
     {
-        float absVelocity = Mathf.Abs(_selfRigidbody.velocity.x);
-
-        if (absVelocity < _maxSpeed)
-        {
-            _selfRigidbody.AddForce(Vector2.right * horizontalInput * _force);
-        }
-
-        if (absVelocity > _maxSpeed)
-        {
-            _selfRigidbody.velocity = new Vector2(Mathf.Sign(_selfRigidbody.velocity.x) * _maxSpeed,
-                                                    _selfRigidbody.velocity.y);
-        }
+        _selfRigidbody.velocity = new Vector2(horizontalInput * _force * Time.deltaTime,
+                                                _selfRigidbody.velocity.y);
     }
 }
